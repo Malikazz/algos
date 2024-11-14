8722cc3cf40654256727a3107b5fc2505d1a24f8
Paul <paul@malikaz.com>
2024-11-13 19:59:32 -0700

    remove target folder

--- rust/leetcode/merge_strings_alernately/target/debug/deps/merge_strings_alernately-99e265daef8432bc.d
+++
@@ -1,5 +0,0 @@
-/home/mal/github/algos/rust/leetcode/merge_strings_alernately/target/debug/deps/libmerge_strings_alernately-99e265daef8432bc.rmeta: src/main.rs
-
-/home/mal/github/algos/rust/leetcode/merge_strings_alernately/target/debug/deps/merge_strings_alernately-99e265daef8432bc.d: src/main.rs
-
-src/main.rs:
--- rust/leetcode/merge_strings_alernately/target/debug/deps/merge_strings_alernately-af44daeb08665ced.d
+++
@@ -1,5 +0,0 @@
-/home/mal/github/algos/rust/leetcode/merge_strings_alernately/target/debug/deps/libmerge_strings_alernately-af44daeb08665ced.rmeta: src/main.rs
-
-/home/mal/github/algos/rust/leetcode/merge_strings_alernately/target/debug/deps/merge_strings_alernately-af44daeb08665ced.d: src/main.rs
-
-src/main.rs:
