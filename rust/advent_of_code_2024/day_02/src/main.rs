ee69ab4d942158a9c96884b6c5898b7af802455f
Paul <paul@malikaz.com>
2024-12-10 21:57:29 -0700

    just not working

--- rust/advent_of_code_2024/day_02/src/main.rs
+++ rust/advent_of_code_2024/day_02/src/main.rs
@@ -1,14 +1,20 @@
-use std::fs;
+use std::{collections::VecDeque, fs};
+use log::debug;
 
-fn main() {
-    println!("partone: {:?}", day_02_part_one());
-    println!("parttwo: {:?}", day_02_part_two());
+enum Direction{
+    Positive,
+    Negative,
+    UnSet
 }
 
-fn day_02_part_one() -> i32 {
-    let file_content = fs::read_to_string("src/input").unwrap();
+fn main() {
+    println!("partone: {:?}", day_02_part_one(parse_input("src/input")));
+    println!("parttwo: {:?}", day_02_part_two(parse_input("src/input")));
+}
+
+fn parse_input(path: &str) -> Vec<Vec<i32>>{
+    let file_content = fs::read_to_string(path).unwrap();
     let mut report_levels: Vec<Vec<i32>> = Vec::new();
-    let mut safe_reports = 0;
 
     // We could do all the logic here but could make part two hard to lets just incure
     // the cost of breaking it into two passes
@@ -20,6 +26,31 @@ fn day_02_part_one() -> i32 {
         report_levels.push(temp);
     }
 
+    report_levels
+}
+
+fn calculate_diff_array(input: Vec<i32>) -> Vec<i32>{
+    let mut output:Vec<i32> = Vec::new();
+    for item in input.windows(2){
+        output.push(item[1] - item[0]);
+    }
+    output
+}
+
+fn follows_rules(input: Vec<i32>) -> bool{
+    let mut output: Vec<i32> = Vec::new();
+    
+    // Dose it follow the one direction rule
+    let direction: 
+    for item in input.iter(){
+         
+    }
+
+    true
+}
+
+fn day_02_part_one(report_levels: Vec<Vec<i32>>) -> i32 {
+    let mut safe_reports = 0;
     'outer: for report in report_levels.iter() {
         let mut even_odd: i32 = -1;
         for numbers in report.windows(2) {
@@ -40,7 +71,6 @@ fn day_02_part_one() -> i32 {
                 }
                 continue;
             } else {
-                println!("List failed at index {:?}, report {:?}", index, report);
                 continue 'outer;
             }
         }
@@ -49,54 +79,34 @@ fn day_02_part_one() -> i32 {
     safe_reports
 }
 
-fn day_02_part_two() -> i32 {
-    let file_content = fs::read_to_string("src/input").unwrap();
-    let mut report_levels: Vec<Vec<i32>> = Vec::new();
-    let mut safe_reports = 0;
+enum Direciton{
+    Up,
+    Down,
+    UnSet
+}
 
-    // We could do all the logic here but could make part two hard to lets just incure
-    // the cost of breaking it into two passes
-    for line in file_content.lines() {
-        let mut temp: Vec<i32> = Vec::new();
-        for number in line.split(" ") {
-            temp.push(number.parse::<i32>().unwrap());
-        }
-        report_levels.push(temp);
+fn day_02_part_two(report_levels: Vec<Vec<i32>>) -> i32 {
+    let mut safe_reports: i32 = 0;
+    let mut work_que:VecDeque<Vec<i32>> = VecDeque::new();
+    
+    for item in report_levels.into_iter(){
+        work_que.push_back(item);
     }
 
-    'outer: for report in report_levels.iter_mut() {
-        let mut even_odd: i32 = -1;
-        let mut one_bad = 0;
-        for index in (1..report.len()).rev() {
-            let temp_left = report[index];
-            let temp_right = report[index - 1];
-            if temp_left < temp_right
-                && ((temp_left - temp_right).abs() <= 3 && (temp_left - temp_right).abs() > 0)
-                && (even_odd == 0 || even_odd == -1)
-            {
-                if even_odd == -1 {
-                    even_odd = 0;
-                }
-                continue;
-            } else if temp_right < temp_left
-                && ((temp_left - temp_right).abs() <= 3 && (temp_left - temp_right).abs() > 0)
-                && (even_odd == -1 || even_odd == 1)
-            {
-                if even_odd == -1 {
-                    even_odd = 1;
-                }
-                continue;
-            } else {
-                if one_bad == 0 {
-                    one_bad = 1;
-                    report.remove(index - 1);
-                    continue;
-                } else {
-                    continue 'outer;
-                }
-            }
+    //evaluate if it passes all the rules
+    while let Some(report) = work_que.pop_front(){
+        let mut direction: Direciton = Direciton::UnSet; 
+        for level in report.windows(3){
+           // does 0 -> 1 follow the rules
+           if (level[0] - level[1]).abs() > 0 && (level[0] - level[1]).abs() < 3 {
+               if level[0] < level[1]{
+                   if let Direciton::UnSet = direction{
+                       direction = Direciton::Up;
+                   }
+                
+               } 
+           }
         }
-        safe_reports += 1;
     }
     safe_reports
 }
@@ -107,11 +117,11 @@ mod tests {
 
     #[test]
     fn test_one() {
-        assert_eq!(day_02_part_one(), 2);
+        assert_eq!(day_02_part_one(parse_input("src/input_test")), 2);
     }
 
     #[test]
     fn test_two() {
-        assert_eq!(day_02_part_two(), 4);
+        assert_eq!(day_02_part_two(parse_input("src/input_test")), 4);
     }
 }
---
+++ rust/advent_of_code_2024/day_03/src/main.rs
@@ -0,0 +1,92 @@
+use regex::bytes::Regex;
+use std::fs::read_to_string;
+
+fn main() {
+    println!("{:?}", part_one(load_input("src/input")));
+    println!("{:?}", part_two(load_input("src/input")));
+}
+
+fn load_input(path: &str) -> String {
+    read_to_string(path).unwrap()
+}
+
+fn part_one(value: String) -> i32 {
+    let regex: Regex = Regex::new(r"mul[(](\d+),(\d+)[)]").unwrap();
+    let mut current_sum: i32 = 0;
+    let instructions: Vec<(String, String)> = regex
+        .captures_iter(value.as_bytes())
+        .filter_map(|cap| {
+            // Convert the captures into &str
+            let key = std::str::from_utf8(&cap[1]).ok()?.to_string();
+            let value = std::str::from_utf8(&cap[2]).ok()?.to_string();
+            Some((key, value))
+        })
+        .collect();
+    for int in instructions.iter() {
+        current_sum += int.0.parse::<i32>().unwrap() * int.1.parse::<i32>().unwrap();
+    }
+
+    current_sum
+}
+
+// Apparently rust does not support variable length negative lockheads so lets just modify the
+// string
+fn pre_process(value: String) -> String {
+    let mut prevous_end: usize = 0;
+    let regex: Regex = Regex::new(r"don't\(\).*?do\(\)").unwrap();
+    let mut clean_str: String = String::with_capacity(value.len());
+
+    for mat in regex.find_iter(value.as_bytes()) {
+        if prevous_end == 0 {
+            clean_str.push_str(&value[0..mat.start()]);
+            println!("Appending {:?}\n", &value[0..mat.start()]);
+            println!("skipping {:?}\n", &value[mat.start()..mat.end()]);
+            prevous_end = mat.end();
+            continue;
+        }
+        clean_str.push_str(&value[prevous_end..mat.start()]);
+        println!("appending {:?}\n", &value[prevous_end..mat.end()]);
+        println!("skipping {:?}\n", &value[mat.start()..mat.end()]);
+        prevous_end = mat.end();
+    }
+
+    clean_str.push_str(&value[prevous_end..]);
+    println!("appending {:?}\n", &value[prevous_end..]);
+    
+    String::from(clean_str)
+}
+
+fn part_two(value: String) -> i32 {
+    let value = pre_process(value);
+    let regex: Regex = Regex::new(r"mul[(](\d+),(\d+)[)]").unwrap();
+    let mut current_sum: i32 = 0;
+    let instructions: Vec<(String, String)> = regex
+        .captures_iter(value.as_bytes())
+        .filter_map(|cap| {
+            // Convert the captures into &str
+            let key = std::str::from_utf8(&cap[1]).ok()?.to_string();
+            let value = std::str::from_utf8(&cap[2]).ok()?.to_string();
+            Some((key, value))
+        })
+        .collect();
+    for int in instructions.iter() {
+        current_sum += int.0.parse::<i32>().unwrap() * int.1.parse::<i32>().unwrap();
+    }
+
+    current_sum
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_pre_process(){
+        assert_eq!(pre_process(String::from("this don't()sadfasdfasdfsdafdo()is a don't()asdfasdfdon't()do()test")), "this is a test");
+    }
+
+    #[test]
+    fn test_pre_process_02(){
+        assert_eq!(pre_process(String::from("don't()Do()asdfsdafsddo()this is a test")), "this is a test");
+    }
+}
