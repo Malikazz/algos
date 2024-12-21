25bf156a56088a53cf89bda90dcd3607770a787d
Paul <paul@malikaz.com>
2024-12-21 14:55:44 -0700

    part way done 6-2

--- rust/advent_of_code_2024/day_06/src/main.rs
+++ rust/advent_of_code_2024/day_06/src/main.rs
@@ -1,41 +1,35 @@
 use std::fs;
 
 fn main() {
-    let part_one_input = parse_input("src/input_test");
+    let part_one_input = parse_input("src/input");
     println!(
         "Part_one: {:?}",
         part_one(part_one_input.0, part_one_input.1)
     );
+    
+    let part_two_input = parse_input("src/input_test");
+    println!(
+        "Part_two: {:?}",
+        part_two(part_two_input.0, part_two_input.1)
+    );
 }
 
-pub struct Movement {
-    pub up: (i32, i32),
-    pub right: (i32, i32),
-    pub down: (i32, i32),
-    pub left: (i32, i32),
-}
+const UP: (i32, i32) = (-1, 0);
+const RIGHT: (i32, i32) = (0, 1);
+const DOWN: (i32, i32) = (1, 0);
+const LEFT: (i32, i32) = (0, -1);
 
-impl Movement {
-    fn new() -> Self {
-        Movement {
-            up: (0, 1),
-            right: (1, 0),
-            down: (0, -1),
-            left: (-1, 0),
-        }
-    }
-    fn rotate_90(rotate_from: (i32, i32), instance: Movement) -> (i32, i32){
-        if rotate_from == instance.up{
-            instance.right
-        } else if rotate_from == instance.right {
-            instance.down
-        } else if rotate_from == instance.down {
-            instance.left
-        } else if rotate_from == instance.left{
-            instance.up
-        } else {
-            panic!("no rotation");
-        }
+fn rotate_90(rotate_from: (i32, i32)) -> (i32, i32) {
+    if rotate_from == UP {
+        RIGHT
+    } else if rotate_from == RIGHT {
+        DOWN
+    } else if rotate_from == DOWN {
+        LEFT
+    } else if rotate_from == LEFT {
+        UP
+    } else {
+        panic!("no rotation");
     }
 }
 
@@ -64,23 +58,113 @@ fn parse_input(path: &str) -> (Vec<Vec<String>>, (usize, usize)) {
 }
 
 fn determine_direciton(value: &str) -> (i32, i32) {
-    let movement = Movement::new();
     match value {
-        "^" => movement.up,
-        ">" => movement.right,
-        "<" => movement.left,
-        "v" => movement.down,
+        "^" => UP,
+        ">" => RIGHT,
+        "<" => LEFT,
+        "v" => DOWN,
         _ => panic!("movement not supported"),
     }
 }
 
-fn part_one(values: Vec<Vec<String>>, guard: (usize, usize)) -> i32 {
-    let sum: i32 = 0;
-    let path_finding = true;
-    let starting_direction = determine_direciton(&values[guard.0][guard.1][0..]);
-    while(path_finding){
-
+fn part_one(matrix: Vec<Vec<String>>, guard: (usize, usize)) -> i32 {
+    let mut matrix = matrix.clone();
+    let mut guard = guard.clone();
+    let mut path_finding = true;
+    let mut direction = determine_direciton(&matrix[guard.0][guard.1][0..]);
+    let mut count: i32 = 0;
+    while path_finding || count == 100000 {
+        count += 1;
+        let next_move_index:(i32, i32) = (guard.0 as i32 + direction.0, guard.1 as i32 + direction.1);
+        if next_move_index.0 < 0 || next_move_index.1 < 0 {
+            path_finding = false;
+            continue;
+        }
+        let fake_vec: Vec<String> = Vec::new();
+        let next_move = matrix.get(next_move_index.0 as usize).unwrap_or(&fake_vec).get(next_move_index.1 as usize);
+        if next_move.is_some(){
+            let next_move_string = matrix[next_move_index.0 as usize][next_move_index.1 as usize].clone();
+            match &next_move_string[0..]{
+                "#" => direction = rotate_90(direction),
+                "." | "X" => {
+                    matrix[next_move_index.0 as usize][next_move_index.1 as usize] = matrix[guard.0][guard.1].clone();
+                    matrix[guard.0][guard.1] = String::from("X");
+                    guard.0 = next_move_index.0 as usize;
+                    guard.1 = next_move_index.1 as usize;
+                },
+                _ => panic!("guard could not move")
+            }
+        } else {
+            path_finding = false;
+        }
     }
+    let mut sum = 1;
+    for items in matrix.iter(){
+        for item in items.iter(){
+            if item == "X"{
+                sum += 1;
+            }
+        }
+    }
+    sum
+}
 
+fn print_matrix(matrix: Vec<Vec<String>>){
+    println!("");
+    for items in matrix.iter(){
+        for item in items.iter(){
+            println!("{:?}", item);
+        }
+    }
+}
+
+
+fn part_two(matrix: Vec<Vec<String>>, guard: (usize, usize)) -> i32 {
+    let mut matrix = matrix.clone();
+    let mut guard = guard.clone();
+    let mut path_finding = true;
+    let mut direction = determine_direciton(&matrix[guard.0][guard.1][0..]);
+    let mut count: i32 = 0;
+    let mut sum = 0;
+    while path_finding || count == 100000 {
+
+        count += 1;
+        let next_move_index:(i32, i32) = (guard.0 as i32 + direction.0, guard.1 as i32 + direction.1);
+        if next_move_index.0 < 0 || next_move_index.1 < 0 {
+            path_finding = false;
+            continue;
+        }
+        let fake_vec: Vec<String> = Vec::new();
+        let next_move = matrix.get(next_move_index.0 as usize).unwrap_or(&fake_vec).get(next_move_index.1 as usize);
+        if next_move.is_some(){
+            let next_move_string = matrix[next_move_index.0 as usize][next_move_index.1 as usize].clone();
+            match &next_move_string[0..]{
+                "#" => direction = rotate_90(direction),
+                "." => {
+                    matrix[next_move_index.0 as usize][next_move_index.1 as usize] = matrix[guard.0][guard.1].clone();
+                    matrix[guard.0][guard.1] = String::from("X");
+                    guard.0 = next_move_index.0 as usize;
+                    guard.1 = next_move_index.1 as usize;
+                },
+                "X" => {
+                    let check_loop = rotate_90(direction);
+                    let check_pos = matrix.get(check_loop.0 as usize + next_move_index.0 as usize).unwrap_or(&fake_vec).get(check_loop.1 as usize + next_move_index.1 as usize);
+                    if check_pos.is_some(){
+                        print_matrix(matrix.clone());
+                        if check_pos.unwrap() == "X"{
+                            sum += 1;
+                        }
+                    }
+                    matrix[next_move_index.0 as usize][next_move_index.1 as usize] = matrix[guard.0][guard.1].clone();
+                    matrix[guard.0][guard.1] = String::from("X");
+                    guard.0 = next_move_index.0 as usize;
+                    guard.1 = next_move_index.1 as usize;
+                },
+                _ => panic!("guard could not move")
+            }
+        } else {
+            path_finding = false;
+        }
+    }
     sum
 }
