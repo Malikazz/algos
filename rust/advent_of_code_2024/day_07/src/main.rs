75d26861d390265a04b767f71f5e2936bd7d8441
Paul Mana <pmana@lawdepot.com>
2024-12-23 10:58:14 -0700

    day 07

--- rust/advent_of_code_2024/day_07/src/main.rs
+++ rust/advent_of_code_2024/day_07/src/main.rs
@@ -4,6 +4,7 @@ use std::{fs, u128};
 // feels alot like the knapsack problem so maybe memoization
 fn main() {
     println!("{:?}", part_one(parse_input("src/input")));
+    println!("{:?}", part_two(parse_input("src/input")));
 }
 
 fn parse_input(path: &str) -> Vec<(u128, Vec<u128>)> {
@@ -23,32 +24,51 @@ fn parse_input(path: &str) -> Vec<(u128, Vec<u128>)> {
     output
 }
 
+//(target, [23,23,23,23])
+
 fn part_one(values: Vec<(u128, Vec<u128>)>) -> u128 {
-    let mut sum:u128 = 0;
-    let maths = vec!["+", "x"];
+    let mut sum: u128 = 0;
+    let maths = vec!["x", "+"];
 
     'outer: for line in values.iter() {
-        // is it to large or to small
-        if line.1.iter().sum::<u128>() > line.0 || line.1.iter().product::<u128>() < line.0 {
-            //skip
-            continue;
-        }
-
         // ok we have to process then
         for perm in repeat_n(maths.iter().cloned(), line.1.len() - 1).multi_cartesian_product() {
             let mut current_sum: u128 = line.1[0];
             for (index, operator) in perm.iter().enumerate() {
                 match operator {
                     &"+" => current_sum += line.1[index + 1],
-                    &"x" => {
-                        current_sum *= line.1[index + 1]
-                    }
+                    &"x" => current_sum *= line.1[index + 1],
                     _ => panic!("should never reach here"),
                 }
-                if current_sum == line.0 {
-                    sum += current_sum;
-                    continue 'outer;
+            }
+            if current_sum == line.0 {
+                sum += current_sum;
+                continue 'outer;
+            }
+        }
+    }
+    sum
+}
+
+fn part_two(values: Vec<(u128, Vec<u128>)>) -> u128 {
+    let mut sum: u128 = 0;
+    let maths = vec!["x", "+", "||"];
+
+    'outer: for line in values.iter() {
+        // ok we have to process then
+        for perm in repeat_n(maths.iter().cloned(), line.1.len() - 1).multi_cartesian_product() {
+            let mut current_sum: u128 = line.1[0];
+            for (index, operator) in perm.iter().enumerate() {
+                match operator {
+                    &"+" => current_sum += line.1[index + 1],
+                    &"x" => current_sum *= line.1[index + 1],
+                    &"||" => current_sum = (current_sum.to_string() + &line.1[index + 1].to_string()).parse::<u128>().unwrap(),
+                    _ => panic!("should never reach here"),
                 }
+            }
+            if current_sum == line.0 {
+                sum += current_sum;
+                continue 'outer;
             }
         }
     }
