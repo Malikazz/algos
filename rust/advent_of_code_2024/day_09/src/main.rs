acbbf24c134b313c572dc3244e711d7871800b67
Paul <paul@malikaz.com>
2024-12-23 21:49:23 -0700

    got to tired to solve this right

--- rust/advent_of_code_2024/day_09/src/main.rs
+++ rust/advent_of_code_2024/day_09/src/main.rs
@@ -1,12 +1,14 @@
 use std::{collections::VecDeque, fs};
 
 fn main() {
-    println!("Part one {:?}", part_one(parse_input("src/input")));
+    println!("Part one {:?}", part_one(parse_input("src/input_test")));
+    println!("Part two {:?}", part_two(parse_input("src/input_test")));
 }
 
 #[derive(Debug)]
 pub struct File {
     pub data: Vec<String>,
+    pub index: usize,
 }
 
 impl File {
@@ -34,7 +36,10 @@ fn calculate_checksum(numbers: Vec<File>) -> usize {
 fn parse_input(path: &str) -> VecDeque<File> {
     let mut output: VecDeque<File> = VecDeque::new();
     for (index, char) in fs::read_to_string(path).unwrap().trim().chars().enumerate() {
-        let mut temp: File = File { data: Vec::new() };
+        let mut temp: File = File {
+            data: Vec::new(),
+            index,
+        };
 
         if index % 2 == 0 || index == 0 {
             for _ in 0..(char.to_string().parse::<usize>().unwrap()) {
@@ -58,7 +63,6 @@ fn parse_input(path: &str) -> VecDeque<File> {
             }
         }
     }
-    //println!("{:?}", output);
     output
 }
 
@@ -90,14 +94,59 @@ fn part_one(hard_drive: VecDeque<File>) -> usize {
             defraged_drive.push(next_empty);
             break;
         }
-        
+
         if next_empty.get_avaiable_space() > 0 {
             hard_drive.push_front(next_empty);
-        }else{
+        } else {
             defraged_drive.push(next_empty);
         }
-
     }
     //println!("{:?}", defraged_drive);
+    calculate_checksum(defraged_drive)
+}
+
+fn part_two(hard_drive: VecDeque<File>) -> usize {
+    let mut hard_drive = hard_drive;
+    let mut defraged_drive: Vec<File> = Vec::new();
+    let mut count = hard_drive.len();
+    while let Some(mut current_back) = hard_drive.pop_back() {
+        count -= 1;
+        if hard_drive.len() == 0 {
+            defraged_drive.push(current_back);
+            break;
+        }
+
+        let size_requirement = current_back.data.len() - current_back.get_avaiable_space();
+        // if some how the current block is all blank just add it to the output
+        if size_requirement == 0 {
+            defraged_drive.push(current_back);
+            continue;
+        }
+        
+        // need a base case
+        let mut moved = false;
+        for index in 0..hard_drive.len(){
+            if hard_drive[index].get_avaiable_space() >= size_requirement{
+                for replace_index in 0..hard_drive[index].data.len(){
+                    if hard_drive[index].data[replace_index] == String::from("."){
+                        if let Some(value) = current_back.data.pop(){
+                            moved = true;
+                            hard_drive[index].data[replace_index] = value;
+                        }
+                    }
+                }
+            }    
+        }
+        
+        if moved == false && count == 0{
+            // drain the vec
+            while let Some(value) = hard_drive.pop_front(){
+                defraged_drive.push(value);
+            }
+            defraged_drive.push(current_back);
+        }
+    }
+    defraged_drive.sort_by(|a, b| a.index.cmp(&b.index));
+    defraged_drive.iter().map(|a| println!("{:?}", a)).collect::<Vec<_>>();
     calculate_checksum(defraged_drive)
 }
